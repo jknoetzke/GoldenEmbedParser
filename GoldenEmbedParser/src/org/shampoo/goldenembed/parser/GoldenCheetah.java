b9633d05d5b66ab96685a4cbec26426cf69e5d94
Justin Knotzke <jknotzke@shampoo.ca>
2010-01-13 18:16:51 -0500

    Removed remaining tabs for spaces.

--- GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GoldenCheetah.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GoldenCheetah.java
@@ -2,51 +2,51 @@ package org.shampoo.goldenembed.parser;
 
 public class GoldenCheetah 
 {
-	//<sample cad="0" watts="0" secs="0" hr="92" len="1" />
+     //<sample cad="0" watts="0" secs="0" hr="92" len="1" />
 
-	private int cad;
-	private int watts;
-	private int secs;
-	private int prevsecs;
-	public int getPrevsecs() {
-		return prevsecs;
-	}
-	public void setPrevsecs(int prevsecs) {
-		this.prevsecs = prevsecs;
-	}
-	private int hr;
-	private int len;
+     private int cad;
+     private int watts;
+     private int secs;
+     private int prevsecs;
+     public int getPrevsecs() {
+          return prevsecs;
+     }
+     public void setPrevsecs(int prevsecs) {
+          this.prevsecs = prevsecs;
+     }
+     private int hr;
+     private int len;
 
-	public int getCad() {
-		return cad;
-	}
-	public void setCad(int cad) {
-		this.cad = cad;
-	}
-	public int getWatts() {
-		return watts;
-	}
-	public void setWatts(int watts) {
-		this.watts = watts;
-	}
-	public int getSecs() {
-		return secs;
-	}
-	public void setSecs(int secs) {
-		this.secs = secs;
-	}
-	public int getHr() {
-		return hr;
-	}
-	public void setHr(int hr) {
-		this.hr = hr;
-	}
-	public int getLen() {
-		return len;
-	}
-	public void setLen(int len) {
-		this.len = len;
-	}
-	
+     public int getCad() {
+          return cad;
+     }
+     public void setCad(int cad) {
+          this.cad = cad;
+     }
+     public int getWatts() {
+          return watts;
+     }
+     public void setWatts(int watts) {
+          this.watts = watts;
+     }
+     public int getSecs() {
+          return secs;
+     }
+     public void setSecs(int secs) {
+          this.secs = secs;
+     }
+     public int getHr() {
+          return hr;
+     }
+     public void setHr(int hr) {
+          this.hr = hr;
+     }
+     public int getLen() {
+          return len;
+     }
+     public void setLen(int len) {
+          this.len = len;
+     }
+     
 
 }
--- GoldenEmbedParser/src/org/shampoo/goldenembed/parser/Power.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/parser/Power.java
@@ -2,57 +2,57 @@ package org.shampoo.goldenembed.parser;
 
 public class Power 
 {
-	private double watts;
-	private double nm;
-	private double rpm;
-	private double p;
-	private double t;
-	private double r;
-	
-	public double getR() {
-		return r;
-	}
-	public void setR(double r) {
-		this.r = r;
-	}
-	boolean first12 = true;
-	public double getWatts() {
-		return watts;
-	}
-	public void setWatts(double watts) {
-		this.watts = watts;
-	}
-	public double getNm() {
-		return nm;
-	}
-	public void setNm(double nm) {
-		this.nm = nm;
-	}
-	public double getRpm() {
-		return rpm;
-	}
-	public void setRpm(double rpm) {
-		this.rpm = rpm;
-	}
-	public double getP() {
-		return p;
-	}
-	public void setP(double p) {
-		this.p = p;
-	}
-	public double getT() {
-		return t;
-	}
-	public void setT(double t) {
-		this.t = t;
-	}
-	public boolean isFirst12() {
-		return first12;
-	}
-	public void setFirst12(boolean first12) {
-		this.first12 = first12;
-	}
-	
-	
+     private double watts;
+     private double nm;
+     private double rpm;
+     private double p;
+     private double t;
+     private double r;
+     
+     public double getR() {
+          return r;
+     }
+     public void setR(double r) {
+          this.r = r;
+     }
+     boolean first12 = true;
+     public double getWatts() {
+          return watts;
+     }
+     public void setWatts(double watts) {
+          this.watts = watts;
+     }
+     public double getNm() {
+          return nm;
+     }
+     public void setNm(double nm) {
+          this.nm = nm;
+     }
+     public double getRpm() {
+          return rpm;
+     }
+     public void setRpm(double rpm) {
+          this.rpm = rpm;
+     }
+     public double getP() {
+          return p;
+     }
+     public void setP(double p) {
+          this.p = p;
+     }
+     public double getT() {
+          return t;
+     }
+     public void setT(double t) {
+          this.t = t;
+     }
+     public boolean isFirst12() {
+          return first12;
+     }
+     public void setFirst12(boolean first12) {
+          this.first12 = first12;
+     }
+     
+     
 
 }
