1f197a7687a9eb464d7c16fbea95e13c24662d7d
Justin Knotzke <jknotzke@shampoo.ca>
2010-02-06 09:54:30 -0500

    White space fix

--- GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GoldenEmbedParserMain.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GoldenEmbedParserMain.java
@@ -714,10 +714,10 @@ public class GoldenEmbedParserMain {
 
     public static double Round(double Rval, int Rpl)
     {
-    	  double p = (double)Math.pow(10,Rpl);
-    	  Rval = Rval * p;
-    	  double tmp = Math.round(Rval);
-    	  return (double)tmp/p;
+          double p = (double)Math.pow(10,Rpl);
+          Rval = Rval * p;
+          double tmp = Math.round(Rval);
+          return (double)tmp/p;
     }
 
 }
