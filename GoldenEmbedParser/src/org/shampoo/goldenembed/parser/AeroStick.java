b4c4788693d6ab7bcaa35aee611875f5d4f6abb0
jknotzke <jknotzke@192.168.1.3>
2010-12-19 09:28:37 -0500

    Aerostick support.

---
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/parser/AeroStick.java
@@ -0,0 +1,35 @@
+package org.shampoo.goldenembed.parser;
+
+public class AeroStick 
+{
+    double windSpeed;
+    double yaw;
+    int windSpeedCounter;
+    int yawCounter;
+    
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+    public double getYaw() {
+        return yaw;
+    }
+    public void setYaw(double yaw) {
+        this.yaw = yaw;
+    }
+    public int getWindSpeedCounter() {
+        return windSpeedCounter;
+    }
+    public void setWindSpeedCounter(int windSpeedCounter) {
+        this.windSpeedCounter = windSpeedCounter;
+    }
+    public int getYawCounter() {
+        return yawCounter;
+    }
+    public void setYawCounter(int yawCounter) {
+        this.yawCounter = yawCounter;
+    }
+
+}
