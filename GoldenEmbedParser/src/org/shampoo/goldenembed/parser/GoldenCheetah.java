7d2d0bc215c94b3fa921497d1f23fb1a3be512b3
Justin Knotzke <jknotzke@shampoo.ca>
2011-02-16 20:15:03 -0500

    Current

--- GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GoldenCheetah.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GoldenCheetah.java
@@ -1,178 +1,214 @@
+/*
+ * Copyright (c) 2009 Justin F. Knotzke (jknotzke@shampoo.ca)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.shampoo.goldenembed.parser;
 
 public class GoldenCheetah {
-	// <sample cad="0" watts="0" secs="0" hr="92" len="1" />
+    // <sample cad="0" watts="0" secs="0" hr="92" len="1" />
 
-	private long cad;
-	private long watts;
-	private long secs;
-	private long prevsecs;
-	private long prevWattsecs;
-	private long prevSpeedSecs;
-	private long prevCadSecs;
-	private long prevHRSecs;
-	private double distance;
-	private double speed;
-	public boolean newWatts = true;
-	private String latitude;
-	private String longitude;
-	private int hr;
-	private int len;
-	private String date;
-	private float elevation;
+    private long cad;
+    private long watts;
+    private long secs;
+    private long prevsecs;
+    private long prevWattsecs;
+    private long prevSpeedSecs;
+    private long prevCadSecs;
+    private long prevHRSecs;
+    private double distance;
+    private double speed;
+    public boolean newWatts = true;
+    private String latitude;
+    private String longitude;
+    private int hr;
+    private int len;
+    private String date;
+    private float elevation;
+    private String description;
+    private String name;
 
-	public float getElevation() {
-		return elevation;
-	}
+    public String getDescription() {
+	return description;
+    }
 
-	public void setElevation(float elevation) {
-		this.elevation = elevation;
-	}
+    public void setDescription(String description) {
+	this.description = description;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public String getName() {
+	return name;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	public int getLen() {
-		return len;
-	}
+    public float getElevation() {
+	return elevation;
+    }
 
-	public void setLen(int len) {
-		this.len = len;
-	}
+    public void setElevation(float elevation) {
+	this.elevation = elevation;
+    }
 
-	public int getHr() {
-		return hr;
-	}
+    public String getDate() {
+	return date;
+    }
 
-	public void setHr(int hr) {
-		this.hr = hr;
-	}
+    public void setDate(String date) {
+	this.date = date;
+    }
 
-	public long getCad() {
-		return cad;
-	}
+    public int getLen() {
+	return len;
+    }
 
-	public void setCad(long cad) {
-		this.cad = cad;
-	}
+    public void setLen(int len) {
+	this.len = len;
+    }
 
-	public long getWatts() {
-		return watts;
-	}
+    public int getHr() {
+	return hr;
+    }
 
-	public void setWatts(long watts) {
-		this.watts = watts;
-	}
+    public void setHr(int hr) {
+	this.hr = hr;
+    }
 
-	public long getSecs() {
-		return secs;
-	}
+    public long getCad() {
+	return cad;
+    }
 
-	public void setSecs(long secs) {
-		this.secs = secs;
-	}
+    public void setCad(long cad) {
+	this.cad = cad;
+    }
 
-	public long getPrevsecs() {
-		return prevsecs;
-	}
+    public long getWatts() {
+	return watts;
+    }
 
-	public void setPrevsecs(long prevsecs) {
-		this.prevsecs = prevsecs;
-	}
+    public void setWatts(long watts) {
+	this.watts = watts;
+    }
 
-	public long getPrevWattsecs() {
-		return prevWattsecs;
-	}
+    public long getSecs() {
+	return secs;
+    }
 
-	public void setPrevWattsecs(long prevWattsecs) {
-		this.prevWattsecs = prevWattsecs;
-	}
+    public void setSecs(long secs) {
+	this.secs = secs;
+    }
 
-	public long getPrevSpeedSecs() {
-		return prevSpeedSecs;
-	}
+    public long getPrevsecs() {
+	return prevsecs;
+    }
 
-	public void setPrevSpeedSecs(long prevSpeedSecs) {
-		this.prevSpeedSecs = prevSpeedSecs;
-	}
+    public void setPrevsecs(long prevsecs) {
+	this.prevsecs = prevsecs;
+    }
 
-	public long getPrevCadSecs() {
-		return prevCadSecs;
-	}
+    public long getPrevWattsecs() {
+	return prevWattsecs;
+    }
 
-	public void setPrevCadSecs(long prevCadSecs) {
-		this.prevCadSecs = prevCadSecs;
-	}
+    public void setPrevWattsecs(long prevWattsecs) {
+	this.prevWattsecs = prevWattsecs;
+    }
 
-	public long getPrevHRSecs() {
-		return prevHRSecs;
-	}
+    public long getPrevSpeedSecs() {
+	return prevSpeedSecs;
+    }
 
-	public void setPrevHRSecs(long prevHRSecs) {
-		this.prevHRSecs = prevHRSecs;
-	}
+    public void setPrevSpeedSecs(long prevSpeedSecs) {
+	this.prevSpeedSecs = prevSpeedSecs;
+    }
 
-	public double getDistance() {
-		return distance;
-	}
+    public long getPrevCadSecs() {
+	return prevCadSecs;
+    }
 
-	public void setDistance(double distance) {
-		this.distance = distance;
-	}
+    public void setPrevCadSecs(long prevCadSecs) {
+	this.prevCadSecs = prevCadSecs;
+    }
 
-	public double getSpeed() {
-		return speed;
-	}
+    public long getPrevHRSecs() {
+	return prevHRSecs;
+    }
 
-	public void setSpeed(double speed) {
-		this.speed = speed;
-	}
+    public void setPrevHRSecs(long prevHRSecs) {
+	this.prevHRSecs = prevHRSecs;
+    }
 
-	public String getLatitude() {
-		return latitude;
-	}
+    public double getDistance() {
+	return distance;
+    }
 
-	public void setLatitude(String latitude) {
-		if (latitude.length() >= 8)
-			this.latitude = latitude.substring(0, 7);
-		else
-			this.latitude = latitude;
-	}
+    public void setDistance(double distance) {
+	this.distance = distance;
+    }
 
-	public String getLongitude() {
-		return longitude;
-	}
+    public double getSpeed() {
+	return speed;
+    }
 
-	public void setLongitude(String longitude) {
-		if (longitude.length() >= 9)
-			this.longitude = longitude.substring(0, 8);
-		else
-			this.longitude = longitude;
-	}
+    public void setSpeed(double speed) {
+	this.speed = speed;
+    }
 
-	public GoldenCheetah clone(GoldenCheetah _gc) {
-		GoldenCheetah gc = new GoldenCheetah();
-		gc.setCad(_gc.getCad());
-		gc.setDistance(_gc.getDistance());
-		gc.setHr(_gc.getHr());
-		gc.setLen(_gc.getLen());
-		gc.setPrevCadSecs(_gc.getPrevCadSecs());
-		gc.setPrevHRSecs(_gc.getPrevHRSecs());
-		gc.setPrevsecs(_gc.getPrevsecs());
-		gc.setPrevSpeedSecs(_gc.getPrevSpeedSecs());
-		gc.setPrevWattsecs(_gc.getPrevWattsecs());
-		gc.setSecs(_gc.getSecs());
-		gc.setSpeed(_gc.getSpeed());
-		gc.setWatts(_gc.getWatts());
-		gc.setLatitude(_gc.getLatitude());
-		gc.setLongitude(_gc.getLongitude());
-		gc.setElevation(_gc.getElevation());
+    public String getLatitude() {
+	return latitude;
+    }
 
-		return gc;
-	}
+    public void setLatitude(String latitude) {
+	if (latitude.length() >= 8)
+	    this.latitude = latitude.substring(0, 7);
+	else
+	    this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+	return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+	if (longitude.length() >= 9)
+	    this.longitude = longitude.substring(0, 8);
+	else
+	    this.longitude = longitude;
+    }
+
+    public GoldenCheetah clone(GoldenCheetah _gc) {
+	GoldenCheetah gc = new GoldenCheetah();
+	gc.setCad(_gc.getCad());
+	gc.setDistance(_gc.getDistance());
+	gc.setHr(_gc.getHr());
+	gc.setLen(_gc.getLen());
+	gc.setPrevCadSecs(_gc.getPrevCadSecs());
+	gc.setPrevHRSecs(_gc.getPrevHRSecs());
+	gc.setPrevsecs(_gc.getPrevsecs());
+	gc.setPrevSpeedSecs(_gc.getPrevSpeedSecs());
+	gc.setPrevWattsecs(_gc.getPrevWattsecs());
+	gc.setSecs(_gc.getSecs());
+	gc.setSpeed(_gc.getSpeed());
+	gc.setWatts(_gc.getWatts());
+	gc.setLatitude(_gc.getLatitude());
+	gc.setLongitude(_gc.getLongitude());
+	gc.setElevation(_gc.getElevation());
+
+	return gc;
+    }
 }
--- GoldenEmbedParser/src/org/shampoo/goldenembed/tools/GnuPlot.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/tools/GnuPlot.java
@@ -10,73 +10,73 @@ import org.shampoo.goldenembed.parser.GoldenCheetah;
 
 public class GnuPlot {
 
-	private final static String SPACE = " ";
+    private final static String SPACE = " ";
 
-	public GnuPlot() {
-	}
+    public GnuPlot() {
+    }
 
-	public void writeOutGnuPlot(List<GoldenCheetah> gcArray, String outFile) {
-		Iterator<GoldenCheetah> iter = gcArray.iterator();
-		GoldenCheetah gc;
-		PrintWriter fout = initPlotFile(outFile);
+    public void writeOutGnuPlot(List<GoldenCheetah> gcArray, String outFile) {
+	Iterator<GoldenCheetah> iter = gcArray.iterator();
+	GoldenCheetah gc;
+	PrintWriter fout = initPlotFile(outFile);
 
-		int totalWatts = 0;
-		int count = 0;
-		int totalHr = 0;
-		int totalCad = 0;
-		int totalSpeed = 0;
-		int totalDistance = 0;
-		// Open up the file..
+	int totalWatts = 0;
+	int count = 0;
+	int totalHr = 0;
+	int totalCad = 0;
+	int totalSpeed = 0;
+	int totalDistance = 0;
+	// Open up the file..
 
-		while (iter.hasNext()) {
-			gc = iter.next();
+	while (iter.hasNext()) {
+	    gc = iter.next();
 
-			if (gc.getSecs() % 20 == 0 && gc.getSecs() != 0) {
+	    if (gc.getSecs() % 20 == 0 && gc.getSecs() != 0) {
 
-				fout.write(convertToTime(gc.getSecs()) + SPACE
-						+ (totalWatts / count) + SPACE + (totalHr / count)
-						+ SPACE + (totalCad / count) + SPACE
-						+ (totalSpeed / count) + SPACE
-						+ (totalDistance / count) + "\n");
+		fout.write(convertToTime(gc.getSecs()) + SPACE
+			+ (totalWatts / count) + SPACE + (totalHr / count)
+			+ SPACE + (totalCad / count) + SPACE
+			+ (totalSpeed / count) + SPACE
+			+ (totalDistance / count) + "\n");
 
-				totalWatts = 0;
-				count = 0;
-				totalHr = 0;
-				totalCad = 0;
-				totalSpeed = 0;
-				totalDistance = 0;
+		totalWatts = 0;
+		count = 0;
+		totalHr = 0;
+		totalCad = 0;
+		totalSpeed = 0;
+		totalDistance = 0;
 
-			}
+	    }
 
-			totalWatts += gc.getWatts();
-			totalHr += gc.getHr();
-			totalCad += gc.getCad();
-			totalSpeed += gc.getSpeed();
-			totalDistance += gc.getDistance();
-			count++;
-
-		}
-		fout.close();
-	}
-
-	private PrintWriter initPlotFile(String outFile) {
-		PrintWriter fout;
-		try {
-			fout = new PrintWriter(new FileOutputStream(outFile));
-			return fout;
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private String convertToTime(long secsIn) {
-		long hours = secsIn / 3600, remainder = secsIn % 3600, minutes = remainder / 60, seconds = remainder % 60;
-
-		return ((hours < 10 ? "0" : "") + hours + ":"
-				+ (minutes < 10 ? "0" : "") + minutes + ":"
-				+ (seconds < 10 ? "0" : "") + seconds);
+	    totalWatts += gc.getWatts();
+	    totalHr += gc.getHr();
+	    totalCad += gc.getCad();
+	    totalSpeed += gc.getSpeed();
+	    totalDistance += gc.getDistance();
+	    count++;
 
 	}
+	fout.close();
+    }
+
+    private PrintWriter initPlotFile(String outFile) {
+	PrintWriter fout;
+	try {
+	    fout = new PrintWriter(new FileOutputStream(outFile));
+	    return fout;
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	return null;
+    }
+
+    private String convertToTime(long secsIn) {
+	long hours = secsIn / 3600, remainder = secsIn % 3600, minutes = remainder / 60, seconds = remainder % 60;
+
+	return ((hours < 10 ? "0" : "") + hours + ":"
+		+ (minutes < 10 ? "0" : "") + minutes + ":"
+		+ (seconds < 10 ? "0" : "") + seconds);
+
+    }
 }
