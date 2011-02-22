dd446cc964bb74c7554e11ac2296fa075bd7915f
Justin Knotzke <jknotzke@shampoo.ca>
2011-02-22 18:42:17 -0500

    Whitespaces

--- GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GPS.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/parser/GPS.java
@@ -2,13 +2,13 @@ package org.shampoo.goldenembed.parser;
 
 public class GPS 
 {
-	private String date;
+  private String date;
     private String time;
     private String latitude= "";
     private String longitude = "";
-	private double speed;
-    
-	public double getSpeed() {
+  private double speed;
+
+  public double getSpeed() {
         return speed;
     }
 
@@ -17,36 +17,35 @@ public class GPS
     }
 
     public String getTime() {
-		return time;
-	}
+    return time;
+  }
 
-	public void setTime(String time) {
-		this.time = time;
-	}
+  public void setTime(String time) {
+    this.time = time;
+  }
 
-	public String getLatitude() {
-		return latitude;
-	}
+  public String getLatitude() {
+    return latitude;
+  }
 
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
 
-	public String getLongitude() {
-		return longitude;
-	}
+  public String getLongitude() {
+    return longitude;
+  }
 
-	public void setLongitude(String longitude) {
-		this.longitude = longitude;
-	}
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
 
-	public String getDate() {
-		return date;
-	}
+  public String getDate() {
+    return date;
+  }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
-	
+  public void setDate(String date) {
+    this.date = date;
+  }
 
 }
--- GoldenEmbedParser/src/org/shampoo/goldenembed/tools/GnuPlot.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/tools/GnuPlot.java
@@ -10,50 +10,50 @@ import org.shampoo.goldenembed.parser.GoldenCheetah;
 
 public class GnuPlot {
 
-	private final static String SPACE = " ";
+  private final static String SPACE = " ";
 
     public GnuPlot() {
     }
 
     public void writeOutGnuPlot(List<GoldenCheetah> gcArray, String outFile) {
-	Iterator<GoldenCheetah> iter = gcArray.iterator();
-	GoldenCheetah gc;
-	PrintWriter fout = initPlotFile(outFile);
+  Iterator<GoldenCheetah> iter = gcArray.iterator();
+  GoldenCheetah gc;
+  PrintWriter fout = initPlotFile(outFile);
 
-	while (iter.hasNext()) {
-	    gc = iter.next();
+  while (iter.hasNext()) {
+      gc = iter.next();
 
-	    if (gc.getSecs() != 0) {
+      if (gc.getSecs() != 0) {
 
-		fout.write(convertToTime(gc.getSecs()) + SPACE
-			+ gc.getWatts() + SPACE + gc.getHr()
-			+ SPACE + gc.getCad() + SPACE
-			+ gc.getSpeed() + SPACE
-			+ gc.getDistance() + SPACE
-			+ gc.getElevation() + "\n");
-	    }
-	}
-	fout.close();
+    fout.write(convertToTime(gc.getSecs()) + SPACE
+      + gc.getWatts() + SPACE + gc.getHr()
+      + SPACE + gc.getCad() + SPACE
+      + gc.getSpeed() + SPACE
+      + gc.getDistance() + SPACE
+      + gc.getElevation() + "\n");
+      }
+  }
+  fout.close();
     }
 
     private PrintWriter initPlotFile(String outFile) {
-	PrintWriter fout;
-	try {
-	    fout = new PrintWriter(new FileOutputStream(outFile));
-	    return fout;
-	} catch (FileNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	return null;
+  PrintWriter fout;
+  try {
+      fout = new PrintWriter(new FileOutputStream(outFile));
+      return fout;
+  } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+  }
+  return null;
     }
 
     private String convertToTime(long secsIn) {
-	long hours = secsIn / 3600, remainder = secsIn % 3600, minutes = remainder / 60, seconds = remainder % 60;
+  long hours = secsIn / 3600, remainder = secsIn % 3600, minutes = remainder / 60, seconds = remainder % 60;
 
-	return ((hours < 10 ? "0" : "") + hours + ":"
-		+ (minutes < 10 ? "0" : "") + minutes + ":"
-		+ (seconds < 10 ? "0" : "") + seconds);
+  return ((hours < 10 ? "0" : "") + hours + ":"
+    + (minutes < 10 ? "0" : "") + minutes + ":"
+    + (seconds < 10 ? "0" : "") + seconds);
 
     }
 
