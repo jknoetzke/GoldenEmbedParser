129bf73a30ecf3230a657e973d0220c46b431bc4
Justin Knotzke <jknotzke@shampoo.ca>
2011-02-27 19:23:50 -0500

    Setup to use Parserlib

--- GoldenEmbedParser/src/org/shampoo/goldenembed/parser/SortBySeconds.java
+++
@@ -1,9 +0,0 @@
-package org.shampoo.goldenembed.parser;
-
-public class SortBySeconds implements java.util.Comparator<GoldenCheetah> {
-    @Override
-    public int compare(GoldenCheetah aGC, GoldenCheetah anotherGC) {
-        long secDiff = aGC.getSecs() - anotherGC.getSecs();
-        return (int) secDiff;
-    }
-}
