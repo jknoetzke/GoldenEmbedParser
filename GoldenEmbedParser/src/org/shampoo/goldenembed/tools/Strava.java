9ce858e1fef2b6792fe908467b4e6b343c3b2a62
Justin Knotzke <jknotzke@shampoo.ca>
2011-09-13 15:14:23 -0400

    Work in progress...

--- GoldenEmbedParser/src/org/shampoo/goldenembed/tools/Strava.java
+++ GoldenEmbedParser/src/org/shampoo/goldenembed/tools/Strava.java
@@ -1,8 +1,13 @@
 package org.shampoo.goldenembed.tools;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
+import java.util.TimeZone;
 
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -19,9 +24,9 @@ public class Strava {
 	WebResource webResource = client.resource("http://www.strava.com/api/v2/");
 	private String token;
 
-	public Strava(List<GoldenCheetah> gcArray) {
-		login();
-		upload(gcArray);
+	public Strava(List<GoldenCheetah> gcArray, String email, String password, String rideDate) {
+		login(email, password);
+		upload(gcArray, rideDate);
 	}
 
 	//public static void main(String[] args) {
@@ -37,13 +42,14 @@ public class Strava {
 		
 	}
 	
-	private void login()
+	private void login(String email, String password)
 	{
 		MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
-		formData.add("email", "jknotzke@shampoo.ca");
-		formData.add("password", "qoOB2JDz");
+		formData.add("email", email);
+		formData.add("password", password);
 		formData.add("agreed_to_terms", "true");
 
+		System.out.println(formData);
 		ClientResponse response = webResource.path("authentication")
 				.path("login").type("application/x-www-form-urlencoded")
 				.post(ClientResponse.class, formData);
@@ -54,7 +60,7 @@ public class Strava {
 
 	}
 	
-	private void upload(List<GoldenCheetah> gcArray)
+	private void upload(List<GoldenCheetah> gcArray, String rideDate)
 	{
 		
 		ArrayList<String> list = new ArrayList<String>();
@@ -65,28 +71,54 @@ public class Strava {
 		list.add("watts");
 		list.add("cadence");
 		list.add("heartrate");
+		list.add("h_accuracy");
+		list.add("v_accuracy");
 		
 		MultivaluedMapImpl formData = new MultivaluedMapImpl();
 		formData.add("token", token);
 		formData.add("type", "json");
 		formData.add("data_fields", list);
 		
-		ArrayList<String> data = new ArrayList<String>();
+		ArrayList<String> data;
 		for (GoldenCheetah gc : gcArray) {
-			data.add(String.valueOf(gc.getSecs()));
+			data = new ArrayList<String>();
+			data.add(formatDate(gc, rideDate));
 			data.add(gc.getLatitude());
 			data.add(gc.getLongitude());
 			data.add(String.valueOf(gc.getElevation()));
 			data.add(String.valueOf(gc.getWatts()));
 			data.add(String.valueOf(gc.getCad()));
 			data.add(String.valueOf(gc.getHr()));
+			formData.add("data", data);
+			break;
 		}
 		
-		formData.add("data", data);
+		System.out.println(formData);
 		ClientResponse response = webResource.path("upload")
 				.type("application/x-www-form-urlencoded")
 				.post(ClientResponse.class, formData);
 		System.out.println(response.getEntity(String.class));
 		
+	}
+	
+	public String formatDate(GoldenCheetah gc, String rideDate)
+	{
+		Calendar rideCal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+		
+		int year = Integer.parseInt(rideDate.substring(0, 4));
+		int month = Integer.parseInt(rideDate.substring(5, 7));
+		int day = Integer.parseInt(rideDate.substring(8, 10));
+		
+		int hours = (int)gc.getSecs() / 3600,
+		remainder = (int)gc.getSecs() % 3600,
+		minutes = remainder / 60,
+		seconds = remainder % 60;
+
+        rideCal.set(year, month, day, hours, minutes, seconds);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+        String formattedDate = sdf.format(rideCal.getTime());
+
+        return formattedDate;
 	}
 }
